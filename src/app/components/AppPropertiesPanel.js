9a6ea24a5281710ea972612fb85397024624db68
Mike Austin <mike_ekim@yahoo.com>
2019-08-06 08:03:55 -0700

    Convert AppPropertiesPanel to class

--- src/app/components/AppPropertiesPanel.js
+++ src/app/components/AppPropertiesPanel.js
@@ -7,8 +7,88 @@ import { NumericInput, NumericField } from 'core/components/NumericInput'
 import PanelHeader from './PanelHeader'
 import { ActionTypes, transformShape, setOpacity } from 'app/actions/common'
 import { shapeRegistration } from 'app/components/CanvasShape'
+import { ShapeList, SelectedShapesContext } from 'app/components'
 
-const AppPropertiesPanel = ({ theme, selectedShapes, setOpacity, transformShape, dispatch }) => {
+class AppPropertiesPanel extends React.PureComponent {
+  handleOpacityValueChange = opacity => {
+    setOpacity(this.props.selectedShapes[0].id, this.props.opacity)
+  }
+
+  handleOpacitySubmit = opacity => {
+    setOpacity(this.props.selectedShapes[0].id, this.props.opacity / 100)
+  }
+
+  handlePositionXSubmit = positionX => {
+    transformShape(this.props.selectedShapes[0].id, ActionTypes.MOVE_SHAPE, {
+      x: positionX - this.props.selectedShapes[0].position.x,
+      y: 0
+    })
+  }
+
+  handlePositionYSubmit = positionY => {
+    transformShape(this.props.selectedShapes[0].id, ActionTypes.MOVE_SHAPE, {
+      x: 0,
+      y: positionY - this.props.selectedShapes[0].position.y
+    })
+  }
+
+  render() {
+    const { theme, selectedShapes } = this.props
+  
+    return (
+      <SelectedShapesContext.Consumer>
+        {xselectedShapes => {
+          const selectedShape = selectedShapes[0] || { position: {}, opacity: null }
+          const disabled = !selectedShapes[0]
+          const opacityProps = { width: 50, maxLength: 3, units: '%', disabled: !selectedShapes[0] }
+          const positionProps = { width: 65, maxLength: 4, units: 'px', disabled: !selectedShapes[0] }
+          const { position, opacity } = selectedShape
+
+          return (
+            <View
+              width={256}
+              style={{marginTop: 1, backgroundColor: theme.backgroundColor}}
+              borderStyle={{borderLeftWidth: 1, left: -1, borderColor: 'hsla(0, 0%, 0%, 0.1)'}}
+            >
+              <PanelHeader heading="Properties" />
+              <View horizontal align="center" style={{ paddingVertical: 5, paddingHorizontal: 10}}>
+                <Slider value={opacity} disabled={disabled} onValueChange={this.handleOpacityValueChange} />
+                <Spacer />
+                <NumericInput {...opacityProps} value={opacity * 100} onSubmit={this.handleOpacitySubmit} />
+              </View>
+              <View horizontal align="center" style={{ paddingVertical: 5, paddingHorizontal: 10}}>
+                <NumericField {...positionProps} label="X" value={position.x} onSubmit={this.handlePositionXSubmit} />
+                <Spacer />
+                <NumericField {...positionProps} label="Y" value={position.y} onSubmit={this.handlePositionYSubmit}
+                />
+              </View>
+              <SectionList
+                renderSectionHeader={({section: {title}}) => (
+                  <View style={{paddingVertical: 5, paddingHorizontal: 10, marginTop: 10}}>
+                    <Text style={{fontWeight: '700'}}>{title}</Text>
+                  </View>
+                )}
+                renderItem={({item, index, section}) => (
+                  <View style={{paddingVertical: 5, paddingHorizontal: 10, backgroundColor: 'white'}}>
+                    <Text key={index}>{item}</Text>
+                  </View>
+                )}
+                keyExtractor={(item, index) => item}
+                sections={[
+                  {title: 'Title1', data: ['item1', 'item2']},
+                  {title: 'Title2', data: ['item3', 'item4']},
+                  {title: 'Title3', data: ['item5', 'item6']},
+                ]}
+              />
+            </View>
+          )
+        }}
+      </SelectedShapesContext.Consumer>
+    )
+  }
+}
+
+const AppPropertiesPanel2 = ({ theme, selectedShapes, setOpacity, transformShape, dispatch }) => {
   const handleOpacityValueChange = useCallback(opacity => {
     setOpacity(selectedShapes[0].id, opacity)
   }, [setOpacity, selectedShapes])
